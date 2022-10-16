d1d9a9032cb238c00383468cc81fdc36527f7fcf
IvanTea71 <108534815+IvanTea71@users.noreply.github.com>
2022-10-16 20:41:22 +0300

    update1

--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject _template;
-
+    [SerializeField] private Enemy _template;
+    [SerializeField] private List<GameObject> _spawns = new List<GameObject>();
+    
     private Coroutine _spawner;
-    private List<GameObject> _spawns = new List<GameObject>();
 
     private void Start()
     {
-        AddPoints();
         CoroutineControl();
-    }
-
-    private void AddPoints()
-    {
-        _spawns.Add(GameObject.Find("GameObject"));
-        _spawns.Add(GameObject.Find("GameObject (1)"));
-        _spawns.Add(GameObject.Find("GameObject (2)"));
-        _spawns.Add(GameObject.Find("GameObject (3)"));
-        _spawns.Add(GameObject.Find("GameObject (4)"));
-        _spawns.Add(GameObject.Find("GameObject (5)"));
     }
 
     private void CoroutineControl()
@@ -39,7 +28,7 @@ public class EnemySpawn : MonoBehaviour
     {
         for (int i = 0; i < _spawns.Count; i++)
         {
-            GameObject enemy = Instantiate(_template, Vector3.zero, Quaternion.identity);
+            Enemy enemy = Instantiate(_template, Vector3.zero, Quaternion.identity);
             Transform enemyTransform = enemy.GetComponent<Transform>();
             enemyTransform.position = _spawns[i].transform.position;
             yield return new WaitForSeconds(2);
